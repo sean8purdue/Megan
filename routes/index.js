60d48758dfd1bd378d75ba273de573409109373b
Sean <sean8purdue@gmail.com>
2017-05-24 15:00:50 -0400

    get user input as parameter

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,13 @@ function sendFlower(req, res, next) {
 
 }
 
+// get user input as parameter
+router.get('/search/:flower', sendFlower2);
+
+function sendFlower2(req, res, next) {
+    var data = req.params;
+    res.send("I love " + data.flower);
+}
+
+
 module.exports = router;
